110c71255b6cd3a5824ee0b6c4d318b43ef34574
Glenn Sun <glenn-sun@outlook.com>
2016-09-04 22:36:00 -0400

    Began scripting

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+	var states = {
+		a01: $('#a01')
+	};
+
+	var progress = {
+		position: {
+			context: "title",
+			page: 0
+		},
+		content: {
+			prologue: false,
+			ch1: false,
+			ch2: false,
+			ch3: false,
+			epilogue: false
+		},
+		quiz: {
+			prologue: false,
+			ch1: false,
+			ch2: false,
+			ch3: false
+		},
+		timeline: {
+			prologue: false,
+			ch1: false,
+			ch2: false,
+			ch3: false,
+			epilogue: false
+		},
+		activitiesComplete: 0
+	}
+
+	var start = function () {
+
+	};
+
+	var gotoPage = function (context, page) {
+
+	};
+
+	var toContent = function (number) {
+
+	};
+
+	var toTimeline = function (number) {
+
+	};
+
+	var toQuiz = function (number) {
+
+	};
+
+	var saveProgress = function () {
+
+	};
+
+	var delProgress = function () {
+
+	};
+});
